5e31da7de45d3c60ae5ce139d922f26e3ed91149
Luca-Mariselli <lucamariselli@gmail.com>
2024-05-27 18:00:31 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+  },
+  mounted() {
+  }
+}
+</script>
+
+<template>
+  <header>
+    <AppHeader />
+  </header>
+
+  <main>
+    <AppMain />
+  </main>
+
+
+</template>
+
+<style scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,43 @@
+<script>
+
+export default {
+    components: {
+
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+    },
+    mounted() {
+    }
+}
+</script>
+
+<template>
+    <header>
+        <img class="logo-w"
+            src="https://vignette1.wikia.nocookie.net/yugiohenespanol/images/a/aa/Yugioh_anime_logo.jpg/revision/latest?cb=20130310042737&path-prefix=es"
+            alt="">
+        <span class="title">Yu-Gi-Oh API</span>
+    </header>
+
+
+
+
+</template>
+
+<style scoped>
+.logo-w {
+    width: 10%;
+    margin-left: 2rem;
+}
+
+.title {
+    font-size: 50px;
+    font-weight: 500;
+    margin-left: 2rem;
+}
+</style>
